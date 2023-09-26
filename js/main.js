fbea01bcc871074de38aa222aefe6efda1840014
Vladyslav Molchan <Nksb2017@gmail.com>
2023-09-26 20:06:29 +0300

    section filter

--- js/main.js
+++ js/main.js
@@ -9,3 +9,27 @@ if (burger && menu) {
   body.classList.toggle('_lock');
 })
 }
+
+
+
+const filter = document.querySelector('.filter');
+
+if (filter) {
+  filter.addEventListener('click', event => {
+    console.log('Click event fired');
+    const blockFilter = event.target.closest('.block-filter');
+    if (!blockFilter) return;
+
+    const dropdown = blockFilter.querySelector('.block-filter__dropdown');
+    const icon = blockFilter.querySelector('.block-filter__icon');
+
+    if (event.target.classList.contains('block-filter__item')) {
+      const valueElement = blockFilter.querySelector('.block-filter__velue');
+      valueElement.textContent = event.target.textContent;
+    }
+
+    dropdown.classList.toggle('_active');
+    icon.classList.toggle('_active');
+  });
+}
+
